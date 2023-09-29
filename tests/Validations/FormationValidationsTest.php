4056176379f04c876cf92f6e46b2de73f62a2f15
squareface <squareface.officiel@gmail.com>
2023-09-29 15:01:16 +0200

    Gestion des tests d'intégration sur la date

---
+++ tests/Validations/FormationValidationsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Validations;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\Formation;
+use DateTime;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class FormationValidationsTest extends KernelTestCase
+{
+    public function getFormation(): Formation{
+        return (new Formation())
+        ->setTitle("Titre de la formation")
+        ->setPublishedAt(new \DateTime("2025-09-2"));
+    }
+
+    public function testValidationModificationFormations(): void
+    {
+        $formation = $this->getFormation()->setPublishedAt(new \DateTime("2025-09-2"));
+        $this->assertErrors($formation, 1, "[ERREUR] 2025-09-2 est postérieure à aujourd'hui");
+    }
+
+    public function assertErrors(Formation $formation, int $nbErreursAttendues, string $message="") {
+        self::bootKernel();
+        $validator = self::getContainer()->get(ValidatorInterface::class);
+        $error = $validator->validate($formation);
+        $this->assertCount($nbErreursAttendues, $error, $message);
+    }
+}
